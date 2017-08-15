124247372fd0b157bb2db2c4280e6479009ade25
Spencer Chan <schan1031@gmail.com>
2017-08-15 16:46:36 -0700

    hashing

--- LRU Cache/Spencer_Chan/lib/p02_hashing.rb
+++ LRU Cache/Spencer_Chan/lib/p02_hashing.rb
@@ -4,11 +4,16 @@ end
 
 class Array
   def hash
+    return 0 if self.empty?
+    self.map(&:hash).reduce(:-)
   end
 end
 
 class String
   def hash
+    alpha = 'abcdefghijklmnopqrstuvwxyz'.chars
+    out = self.chars.map { |el| el = alpha.index(el) }
+    out.hash
   end
 end
 
@@ -16,6 +21,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    out = 0
+    self.keys.each do |k|
+      out += k.hash - self[k].hash
+    end
+    out
   end
 end
