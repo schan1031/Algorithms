ec0b5682c31c1dbf9c4fb1f0ae1f6c1cd6b299cc
Spencer Chan <schan1031@gmail.com>
2017-08-15 16:50:16 -0700

    Hash Set finished

--- LRU Cache/Spencer_Chan/lib/p03_hash_set.rb
+++ LRU Cache/Spencer_Chan/lib/p03_hash_set.rb
@@ -9,18 +9,28 @@ class HashSet
   end
 
   def insert(key)
+    @count += 1
+    if @count > num_buckets
+      resize!
+      @store[key.hash % num_buckets].push(key)
+    else
+      @store[key.hash % num_buckets].push(key)
+    end
   end
 
   def include?(key)
+    @store[key.hash % num_buckets].include?(key)
   end
 
   def remove(key)
+    @store[key.hash % num_buckets].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num]
   end
 
   def num_buckets
@@ -28,5 +38,12 @@ class HashSet
   end
 
   def resize!
+    out = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |arr|
+      arr.each do |el|
+        out[el % out.length].push(el)
+      end
+    end
+    @store = out
   end
 end
