3fba9d76a7495c84ccb43909f7f17eb7d3c7e882
Spencer Chan <schan1031@gmail.com>
2017-08-15 17:32:30 -0700

    linked list done

--- LRU Cache/Spencer_Chan/lib/p04_linked_list.rb
+++ LRU Cache/Spencer_Chan/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Node
   attr_accessor :key, :val, :next, :prev
 
@@ -19,7 +21,13 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Node.new
+    @tail = Node.new
+
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,30 +36,77 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    return true if @tail.prev == @head
+    false
   end
 
   def get(key)
+    node = @head.next
+    while node.key
+      return node.val if node.key == key
+      node = node.next
+    end
+    nil
   end
 
   def include?(key)
+    node = @head.next
+    until node == @tail
+      if node.key == key
+        return true
+      else
+        node = node.next
+      end
+    end
+    false
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    node.prev = @tail.prev
+    @tail.prev.next = node
+    @tail.prev = node
+    node.next = @tail
   end
 
   def update(key, val)
+    node = @head.next
+    while node.key
+      if node.key == key
+        node.val = val
+        return
+      end
+      node = node.next
+    end
   end
 
   def remove(key)
+    node = @head.next
+    until node == @tail
+      if node.key == key
+        # byebug
+        node.prev.next = node.next
+        node.next.prev = node.prev
+      end
+      node = node.next
+    end
+    nil
   end
 
-  def each
+  def each(&prc)
+    node = @head.next
+    until node == @tail
+      prc.call(node)
+      node = node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
