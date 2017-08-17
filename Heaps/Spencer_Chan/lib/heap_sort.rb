02a0f2010308a2f734606c2f0e717b91460ef9c8
Spencer Chan <schan1031@gmail.com>
2017-08-16 21:56:26 -0700

    heap sort finished

--- Heaps/Spencer_Chan/lib/heap_sort.rb
+++ Heaps/Spencer_Chan/lib/heap_sort.rb
@@ -2,5 +2,22 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+
+    heap = BinaryMinHeap.new
+
+    self.each do |el|
+      heap.push(el)
+    end
+
+    sep_ind = 0
+    out = []
+    until heap.count == 0
+      out << heap.extract
+    end
+
+    out.each_index do |idx|
+      self[idx] = out[idx]
+    end
+
   end
 end
