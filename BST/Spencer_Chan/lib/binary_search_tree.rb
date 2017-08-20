6df36795df87c32e2e93faef0b91aa84723a13e9
Spencer Chan <schan1031@gmail.com>
2017-08-19 18:39:53 -0700

    finished BST depth, balance, in order traversal

--- BST/Spencer_Chan/lib/binary_search_tree.rb
+++ BST/Spencer_Chan/lib/binary_search_tree.rb
@@ -31,8 +31,10 @@ class BinarySearchTree
 
     if dir == 'left'
       node.left = BSTNode.new(value)
+      node.left.parent = node
     else
       node.right = BSTNode.new(value)
+      node.right.parent = node
     end
 
   end
@@ -59,8 +61,31 @@ class BinarySearchTree
   def delete(value)
     target = find(value, @root)
     return nil if target == nil
+    if target == @root
+      @root = nil
+      return
+    end
 
-    
+    if child_count(target) == 0
+      set_child(target, target.parent, nil)
+    elsif child_count(target) == 1
+      if target.left
+        set_child(target, target.parent, target.left)
+      else
+        set_child(target, target.parent, target.right)
+      end
+    elsif child_count(target) == 2
+      max = maximum(target.left)
+      if child_count(max) == 0
+        target.value = max.value
+        set_child(max, max.parent, nil)
+      else
+        target.value = max.value
+        set_child(max, max.parent, max.left)
+        max.left.parent = max.parent
+      end
+    end
+
 
   end
 
@@ -73,12 +98,26 @@ class BinarySearchTree
   end
 
   def depth(tree_node = @root)
+    return -1 if tree_node.nil?
+    left = depth(tree_node.left)
+    right = depth(tree_node.right)
+    if left > right
+      return left += 1
+    else
+      return right += 1
+    end
   end
 
   def is_balanced?(tree_node = @root)
+    depth(tree_node.right) == depth(tree_node.left)
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    return if tree_node.nil?
+    in_order_traversal(tree_node.left, arr)
+    arr << tree_node.value
+    in_order_traversal(tree_node.right, arr)
+    arr
   end
 
 
@@ -93,34 +132,35 @@ class BinarySearchTree
     end
   end
 
-  def find_parent(value, tree_node = @root)
-    node = tree_node
-    node2 = node
-    dir = which_way(value, @root)
-
-    while true
-      break if node.value == value
-      node2 == node
-      if dir == 'left'
-        break if node.left == nil
-        node = node.left
-      else
-        break if node.right == nil
-        node = node.right
-      end
-      dir = which_way(value, node)
+  def only_child(node)
+    if node.right
+      return node.right
+    else
+      return node.left
     end
-    return 'root node' if node2 == parent
-    return node2
+  end
+
+  def child_count(node)
+    count = 0
+    count += 1 if node.left
+    count += 1 if node.right
+    return count
   end
 
   def rewire_max(node)
     return if node.left == nil
-    parent = find_parent(node)
+    node.parent.right = node.left
+  end
+
+  def set_child(node, parent, val)
     if parent.right == node
-      parent.right == node.left
+      parent.right = val
+    else
+      parent.left = val
     end
-    nil
+    if val
+      val.parent = parent
+    end
   end
 
 end
--- BST/Spencer_Chan/lib/bst_node.rb
+++ BST/Spencer_Chan/lib/bst_node.rb
@@ -1,6 +1,7 @@
 class BSTNode
-  attr_accessor :left, :right, :value
+  attr_accessor :left, :right, :value, :parent
   def initialize(value)
+    @parent = nil
     @left = nil
     @right = nil
     @value = value
